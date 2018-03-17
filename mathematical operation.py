5330f5ab7402ffb0f5e4003abd486fa5a3257765
Ananthiselvi <37336021+Ananthiselvi@users.noreply.github.com>
2018-03-17 15:59:07 +0530

    Create mathematical operation.py

---
+++ mathematical operation.py
@@ -0,0 +1,23 @@
+MATHEMATICAL OPERATION
+a=4
+b=2
+c=3
+d=1
+e=2
+f=1
+g=2
+h=1
+i=3
+j=2
+sum=a+b+c+d+e+f+g+h+i+j
+minus=a-b-c-d-e-f-g-h-i-j
+mul=a*b*c*d*e*f*g*h*i*j
+div=a/b/c/d/e/f/g/h/i/j
+mod=a%b%c%d%e%f%g%h%i%j
+expo=a**b**c**d**e**f**g**h**i**j
+print sum
+print minus
+print mul
+print div
+print mod
+print expo
