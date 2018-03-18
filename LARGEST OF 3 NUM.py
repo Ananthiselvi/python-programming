64c49e74818413937c233f4254fe3d1f1d27301f
Ananthiselvi <37336021+Ananthiselvi@users.noreply.github.com>
2018-03-18 11:37:17 +0530

    Delete LARGEST OF 3 NUM.py

--- LARGEST OF 3 NUM.py
+++
@@ -1,9 +0,0 @@
-a=input("enter first number: ")
-b=input("enter second number: ")
-c=input("enter third number: ")
-if(a>b and a>c):
-    print('a')
-elif(b>a and b>c):
-     print('b')
-else:
-     print('c')
