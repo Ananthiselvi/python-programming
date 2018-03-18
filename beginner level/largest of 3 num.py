1512b3ace730011d7252b132178826aa51d28cac
Ananthiselvi <37336021+Ananthiselvi@users.noreply.github.com>
2018-03-18 11:26:08 +0530

    Create largest of 3 num.py

---
+++ beginner level/largest of 3 num.py
@@ -0,0 +1,9 @@
+a=input("enter first number: ")
+b=input("enter second number: ")
+c=input("enter third number: ")
+if(a>b and a>c):
+    print('a')
+elif(b>a and b>c):
+     print('b')
+else:
+     print('c')
