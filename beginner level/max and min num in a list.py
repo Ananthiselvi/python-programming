d493f87016ca12c524dd3e22db9b9ecf1145c3d5
Ananthiselvi <37336021+Ananthiselvi@users.noreply.github.com>
2018-04-09 18:54:44 +0530

    Update max and min num in a list.py

--- beginner level/max and min num in a list.py
+++ beginner level/max and min num in a list.py
@@ -1,6 +1,6 @@
-b=[]
-num=int(input("enter the size of the list: "))
-for i in range(num):
-    c=input("enter the numbers: ")
-    b.append(c)
-print(min(b),max(b))
+num=int(input("enter the number in a list: "))
+c=[]
+a=input().split()
+c.append(c)
+print(min(a),max(a))
+        
