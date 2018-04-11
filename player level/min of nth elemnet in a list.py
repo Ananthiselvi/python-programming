d0803982a13a0982eda5861922b73d140c45034b
Ananthiselvi <37336021+Ananthiselvi@users.noreply.github.com>
2018-04-11 18:03:58 +0530

    Update min of nth elemnet in a list.py

--- player level/min of nth elemnet in a list.py
+++ player level/min of nth elemnet in a list.py
@@ -1,6 +1,6 @@
-n=int(input("enter the number: "))
 a=[]
 b=int(input("enter the size of the element: "))
+n=int(input("enter the number: "))
 for i in range(1,b+1):
     c=input("enter the number: ")
     a.append(c)
