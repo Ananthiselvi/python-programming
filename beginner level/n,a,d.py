bd5cb33cc6e6f21e0af675a8624b13ce975d8b29
Ananthiselvi <37336021+Ananthiselvi@users.noreply.github.com>
2018-04-13 17:48:15 +0530

    Create n,a,d.py

---
+++ beginner level/n,a,d.py
@@ -0,0 +1,15 @@
+n=int(input("enter the number: "))
+a=int(input("enter the number: "))
+d=int(input("enter the number: "))
+s=0
+s=s+a
+h=1
+r=s
+g=0
+while(n>1):
+    r=r+(h*d)
+    w=r
+    g=w+g
+    n-=1
+u=s+g
+print(u)
